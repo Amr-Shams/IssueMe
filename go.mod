1aff6d5cf7fbd910b95b421f9fe6c64cd17346c9
amraly <amr.shams2015.as@gmail.com>
2024-07-02 04:33:24 +0300

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Amr-Shams/IssueMe
+
+go 1.21.1
---
+++ todo.go
@@ -0,0 +1,33 @@
+package main 
+import (
+    "fmt"
+    "strings"
+)
+
+type Todo struct {
+    Prefix        string
+    Suffix        string
+    Keyword       string
+    Description   string
+    Uergency      int
+    ID            *string 
+    FileName      string
+    Line          int
+    Title         string
+}
+
+func (t *Todo) String() string {
+    urgencySuffix:=strings.Repeat(string(t.Keyword[len(t.Keyword)-1]), t.Uergency)
+    idStr:=""
+    if t.ID!=nil {
+        idStr=*t.ID
+    }
+   return fmt.Sprintf("%s%s%s(%s): %s",
+		todo.Prefix, todo.Keyword, urgencySuffix, idStr,
+		todo.Suffix)
+}
+
+//TODO: Add a function to update the todo (add issue id, commit message, update file(inplace), etc) 
+
+
+
